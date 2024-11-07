df7e95310ae7d4db90313656087d827eb5750239
EOEboh <ecolejnr007@gmail.com>
2024-11-07 23:17:18 +0100

    accessibility

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,7 +8,6 @@ import {
 } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import CardWrapper from "@/app/ui/dashboard/cards";
-import { CardSkeleton } from "@/app/ui/skeletons";
 
 export default async function Page() {
   return (
@@ -17,7 +16,7 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardSkeleton />}>
+        <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
       </div>
